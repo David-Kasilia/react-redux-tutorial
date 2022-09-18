8616724b10c041df09b09e84b00d593872412d4c
David-Kasilia <davidkasilia001@gmail.com>
2022-09-18 18:20:11 +0300

    Using Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { useSelector, useDispatch } from "react-redux";
+import { actions } from './store/index'
 import "./App.css";
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   const increment = () => {
-    dispatch({ type: "INC" });
+    dispatch(actions.increment());
   };
   const decrement = () => {
-    dispatch({ type: "DEC" });
+    dispatch(actions.decrement());
   };
   const addBy = () => {
-    dispatch({ type: 'ADD', payload: 10})
+    dispatch(actions.addBy(10))
   }
   return (
     <div className="App">
--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,67 @@
-import { legacy_createStore as createStore} from 'redux'
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const reducerFunction= (state={ counter : 0 }, action) => {
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: { counter: 0 },
+    reducers: {
+        increment(state, action) { 
+            state.counter++
+        },
+        decrement(state, action) { 
+            state.counter --
+        },
+        addBy(state, action) { 
+            state.counter += action.payload
+        },
+    }
+})
 
-// Synchronous Function
-// We should not mutate the original state
+export const actions = counterSlice.actions;
 
-if (action.type === 'INC'){
-    return { counter:state.counter + 1 };
-}
-
-if (action.type === 'DEC'){
-    return { counter:state.counter - 1 };
-}
-
-if (action.type === 'ADD'){
-    return { counter:state.counter + action.payload };
-}
-
-
-    return state
-}
-
-const store = createStore(reducerFunction)
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
 
 export default store;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Before Redux Toolkit
+
+// import { legacy_createStore as createStore} from 'redux'
+
+// const reducerFunction= (state={ counter : 0 }, action) => {
+
+// // Synchronous Function
+// // We should not mutate the original state
+
+// if (action.type === 'INC'){
+//     return { counter:state.counter + 1 };
+// }
+
+// if (action.type === 'DEC'){
+//     return { counter:state.counter - 1 };
+// }
+
+// if (action.type === 'ADD'){
+//     return { counter:state.counter + action.payload };
+// }
+
+
+//     return state
+// }
+
+// const store = createStore(reducerFunction)
+
+// export default store;
